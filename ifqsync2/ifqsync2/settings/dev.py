f1b86c437df33b6c348bde9ce9247cd8f5c5584c
Maurizio Branca <maurizio.branca@gmail.com>
2014-11-15 17:22:10 +0100

    Updated db path

--- ifqsync2/ifqsync2/settings/dev.py
+++ ifqsync2/ifqsync2/settings/dev.py
@@ -7,7 +7,7 @@ TEMPLATE_DEBUG = DEBUG
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'dev.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../../db.sqlite3'),
     }
 }
 
